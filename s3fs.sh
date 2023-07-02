eba7c22e6778c8d36a5b2d96c1d2e3b4d0a2d9ea
robertxchen66 <robertxchen66@hotmail.com>
2023-07-02 13:54:55 -0700

    Update s3fs.sh

--- s3fs.sh
+++ s3fs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr/bin/s3fs audiostreamsfrommimosa -o nonempty -o passwd_file=/home/pi/passwd-s3fs /mnt/s3
+sudo /usr/bin/s3fs audiostreamsfrommimosa -o nonempty -o passwd_file=/home/pi/passwd-s3fs -o allow_other /mnt/s3
 /usr/bin/rsync -avz --delete /home/pi/Downloads /mnt/s3
 /bin/umount /mnt/s3
